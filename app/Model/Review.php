a0f24812ac5e426d6a488333533ffc04176a388c
balajimn27 <balajimani270997@gmail.com>
2019-07-16 15:42:43 +0530

    db also added after migration

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -1,10 +1,15 @@
-<?php
+<?php 
 
 namespace App\Model;
+use App\Model\Product;
+use App\Model\Review;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product(){
+
+    	return $this->belongsTo(Product::class);
+    }
 }
